97d55f339c8291afd075e7401b47e95fbae0948c
KavindiViranga <97431820+KavindiViranga@users.noreply.github.com>
2026-03-01 22:35:52 +0530

    push the completed dashbord page

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -2,9 +2,10 @@ import { Activity, useEffect, useState } from "react"
 import { getMotivationalMessage } from "../assets/assets"
 import { useAppContext } from "../context/AppContext"
 import type { ActivityEntry, FoodEntry } from "../types"
-import { ActivityIcon, FlameIcon, HamburgerIcon, User, ZapIcon } from "lucide-react"
+import { ActivityIcon, FlameIcon, HamburgerIcon, RulerIcon, ScaleIcon, TrendingUpIcon, User, ZapIcon } from "lucide-react"
 import Card from "../components/ui/Card"
 import ProgressBar from "../components/ui/ProgressBar"
+import CaloriesChart from "../components/CaloriesChart"
 
 const Dashboard = () => {
 
@@ -164,8 +165,159 @@ const Dashboard = () => {
               <p>activities logged</p>
             <p></p>
           </Card>
-
          </div>
+
+         {/*goal card*/}
+         {user && (
+          <Card className="bg-linear-to-r fro-slate-800 t0-slate-700">
+            <div className="flex items-center gap-4">
+              <div className="w-12 h-12 rounded-xl bg-white/10 flex 
+              items-center justify-center">
+                <TrendingUpIcon className="w-6 h-6 text-emerald-400"/>
+              </div>
+              <div>
+                <p className="text-slate-400 text-sm">Your Goal</p>
+                <p className="dark:text-white font-semibold capitalize">
+                  {user.goal === 'lose' && '🔥 Lose Weight'}
+                  {user.goal === 'maintain' && '⚖️ Maintain Weight'}
+                  {user.goal === 'gain' && '💪 Gain Muscle'}
+                </p>
+              </div>
+            </div>
+          </Card>
+         )}
+         {/*body metrics card*/}
+         {user && user.weight && (
+          <Card>
+            <div className="flex items-center gap-4 mb-6">
+              <div className="w-12 h-12 rounded-xl bg-indigo-100 
+              flex items-center justify-center">
+                <ScaleIcon className="w-16 h-16 text-indigo-500"/>
+              </div>
+              <div>
+                <h3 className="font-semibold text-scale-800 dark:text-white">
+                  Body Metrics</h3>
+                  <p className="text-slate-500 text-sm">Your stats</p>
+              </div>
+            </div>
+
+            <div className="space-y-4">
+              <div className="flex justify-between items-center">
+                <div className="fex items-center gap-2">
+                  <div className="p-1.5 rounded-g bg-slate-100 
+                  dark:bg-slate-800">
+                    <ScaleIcon className="w-4 h-4 text-slate-500"/>
+                  </div>
+                  <span className="text-sm text-slate-500 dark:text-slate-400">
+                    Weight</span>
+                </div>
+                <span className="font-semibold text-slate-700 dark:text-slate-200">
+                  {user.weight} kg</span>
+              </div>
+
+              {user.height && (
+                <div className="flex justify-between items-center">
+                  <div className="fex items-center gap-2">
+                    <div className="p-1.5 rounded-g bg-slate-100 
+                    dark:bg-slate-800">
+                      <RulerIcon className="w-4 h-4 text-slate-500"/>
+                    </div>
+                    <span className="text-sm text-slate-500 dark:text-slate-400">
+                      Height</span>
+                  </div>
+                  <span className="font-semibold text-slate-700 dark:text-slate-200">
+                    {user.height} cm</span>
+                </div>
+              )}
+
+              {user.height && (
+                <div className="pt-2 border-t border-sate-100 
+                dark:border-slate-800">
+                  <div className="flex justify-between items-center mb-2">
+                    <span className="text-sm font-medium text-slate-700 
+                    dark:text-slate-300">BMI</span>
+                    {(() => {
+                      const bmi = (user.weight / Math.pow(user.height / 
+                        100, 2)).toFixed(1);
+                      const getStatus = (b: number) => {
+                        if(b < 18.5) return {
+                          color: 'text-blue-500',
+                          bg: 'bg-blue-500'};
+                          if(b < 25) return {
+                            color: 'text-emerald-500',
+                            bg: 'bg-emerald-500'};
+                            if(b < 30) return {
+                            color: 'text-orange-500',
+                            bg: 'bg-orange-500'};
+
+                            return {
+                              color: 'text-red-500',
+                              bg: 'bg-red-500'};
+                       }
+                      const status = getStatus(Number(bmi));
+                      return <span className={`text-lg font-bold ${status.color}`}>
+                        {bmi}
+                        </span>
+                      })()}
+                    </div>
+
+                    {/*BMI scale*/}
+                    <div className="h-2 w-full bg-slate-100 
+                    dark:bg-slate-800 rounded-ful overflow-hidden flex">
+                   <div className="flex-1 bg-blue-400 opacity-30"></div> 
+                   <div className="flex-1 bg-emerald-400 opacity-30"></div> 
+                   <div className="flex-1 bg-orange-400 opacity-30"></div>
+                   <div className="flex-1 bg-red-400 opacity-30"></div> 
+                   </div>
+                   <div  className="flex justify-between mt-1 text-[10px]
+                   text-slate-400">
+                    <span>18.5</span>
+                    <span>25</span>
+                    <span>30</span>
+                   </div>
+                  </div>
+              )}             
+            </div>
+          </Card>
+         )}
+         {/*summary*/}
+         <Card>
+          <h3 className="font-semibold text-slate-800 dark:text-white mb-4">
+            Today's Summary
+          </h3>
+
+          <div className="space-y-3">
+            <div className="flex justify-between items-center py-2 border-b 
+            border-slate-100 dark:border-slate-800">
+              <span className="text-slate-500 dark:text-sate-400">
+                Meals logged</span>
+              <span className="font-medium text-slate-700 dark:text-slate-200">
+                {todayFood.length}</span>
+            </div>
+
+            <div className="flex justify-between items-center py-2 border-b 
+            border-slate-100 dark:border-slate-800">
+              <span className="text-slate-500 dark:text-sate-400">
+                Total Calories</span>
+              <span className="font-medium text-slate-700 dark:text-slate-200">
+                {totalCalories} kcal</span>
+            </div>
+
+            <div className="flex justify-between items-center py-2">
+              <span className="text-slate-500 dark:text-sate-400">
+                Active Time</span>
+              <span className="font-medium text-slate-700 dark:text-slate-200">
+                {totalActiveMinutes} min</span>
+            </div>
+          </div>
+         </Card>
+
+         {/* graph*/}
+         <Card className="col-span-2">
+          <h3 className="font-semibold text-slate-800 dark:text-white mb-2">
+            This Weeks's Progress</h3>
+            <CaloriesChart />
+         </Card>
       </div>
 
     </div>
